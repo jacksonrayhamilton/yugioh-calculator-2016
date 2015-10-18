0bccec311c7c1a7cf7deab63e1776b92357d8732
Jackson Hamilton <jackson@jacksonrayhamilton.com>
2015-10-17 21:47:56 -0700

    Add reset. Merge layout and cancel into app.

--- app/app.js
+++ app/app.js
@@ -2,33 +2,45 @@
 
 var ycMakeApp = function () {
   var app = {};
-  var players = ycTimes(2, function (n) {
-    return ycMakePersistedPlayer({
-      id: n
+  var players;
+  var operand;
+  var lps;
+  var digits;
+  var initFirst = function () {
+    players = ycTimes(2, function (n) {
+      return ycMakePersistedPlayer({
+        id: n
+      });
     });
-  });
-  var operand = ycMakeOperand();
-  var lps = players.map(function (player) {
-    return ycMakeLp({
-      player: player,
-      operand: operand
+  };
+  var initNth = function () {
+    operand = ycMakeOperand();
+    lps = players.map(function (player) {
+      return ycMakeLp({
+        player: player,
+        operand: operand
+      });
     });
-  });
-  var digits = ycTimes(10, function (n) {
-    return ycMakeDigit({
-      value: n,
-      operand: operand
+    digits = ycTimes(10, function (n) {
+      return ycMakeDigit({
+        value: n,
+        operand: operand
+      });
     });
-  });
-  var cancel = ycMakeCancel({
-    operand: operand
-  });
-  var layout = ycMakeLayout({
-    operand: operand,
-    lps: lps,
-    digits: digits,
-    cancel: cancel
-  });
+  };
+  var init = function () {
+    initFirst();
+    initNth();
+  };
+  init();
+  var reset = function () {
+    players = ycTimes(2, function (n) {
+      return ycMakePlayer({
+        id: n
+      });
+    });
+    initNth();
+  };
   var onKeydown = function (event) {
     var keyCode = event.keyCode;
     if (keyCode === 8) { // backspace
@@ -43,8 +55,30 @@ var ycMakeApp = function () {
       m.endComputation();
     }
   };
+  var cancel = function () {
+    operand.reset();
+  };
+  app.view = function () {
+    return m('.yc-layout', [
+      lps.map(function (lp) {
+        return lp.view();
+      }),
+      m('.yc-layout-row.yc-layout-modeline', [
+        m('.yc-modeline-button.yc-cancel', { onclick: cancel }, 'C'),
+        m('.yc-modeline-button.yc-reset', { onclick: reset }, 'R'),
+        operand.view()
+      ]),
+      ycTimes(2, function (n) {
+        var someDigits = digits.slice(n * 5, (n * 5) + 5);
+        var views = someDigits.map(function (digit) {
+          return digit.view();
+        });
+        return m('.yc-layout-row.yc-layout-digits', views);
+      })
+    ]);
+  };
   document.addEventListener('keydown', onKeydown);
-  m.mount(document.body, { view: layout.view });
+  m.mount(document.body, { view: app.view });
   app.destroy = function () {
     document.removeEventListener('keydown', onKeydown);
     m.mount(document.body, null);
