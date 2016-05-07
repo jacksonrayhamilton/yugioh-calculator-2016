9b2cab87aa60c7e0a7b505c764fa9b321f9e2cb0
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2016-05-06 18:04:19 -0700

    Move require configuration to separate file.

--- app/main.js
+++ app/main.js
@@ -1,35 +1,5 @@
 'use strict';
 
-require.config({
-  paths: {
-    'css': 'node_modules/require-css/css',
-    'm': 'node_modules/mithril/mithril',
-    'FastClick': 'node_modules/fastclick/lib/fastclick',
-    'YC': 'module',
-    'YC App': 'app',
-    'YC Calc': 'calc',
-    'YC Digit': 'digit',
-    'YC Digit Styles': 'digit',
-    'YC Events': 'events',
-    'YC History': 'history',
-    'YC History Styles': 'history',
-    'YC Lp': 'lp',
-    'YC Lp Styles': 'lp',
-    'YC Operand': 'operand',
-    'YC Operand Styles': 'operand',
-    'YC Persistence': 'persistence',
-    'YC Player': 'player',
-    'YC Time': 'time',
-    'YC Timer': 'timer',
-    'YC Timer Styles': 'timer',
-    'YC Undos': 'undos',
-    'YC Utils': 'utils',
-    'YC Global Styles': 'global',
-    'YC Button Styles': 'button',
-    'YC Layout Styles': 'layout'
-  }
-});
-
 require([
   'YC',
   'YC App',
