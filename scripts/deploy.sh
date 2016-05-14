8c74dca71ca6dc1879926249952db43ca8cb3928
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2016-05-14 16:29:29 -0700

    Don't delete old files on deploy.
    This could cause downtime and is usually unnecessary.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,7 @@ ssh_login=ubuntu@ec2-54-67-17-42.us-west-1.compute.amazonaws.com
 # Build and copy client files.
 grunt build
 
-# Cleanup old files.
-ssh ${ssh_login} <<EOF
-rm -rf ~/yc/
-mkdir -p ~/yc/
-EOF
-
 # Package the application and extract it on the server.
 tar --verbose --create --gzip build/ \
     | ssh ${ssh_login} \
-          'tar --verbose --extract --gzip -C ~/yc/'
+          'mkdir -p ~/yc/ && tar --verbose --extract --gzip -C ~/yc/'
