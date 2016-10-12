6bdbf24de1db560c5de838735de53d2dcf840a1e
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2016-10-12 01:02:07 -0700

    Add favicons and HTML minification.

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+/* eslint-env node */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-favicons');
+
+  grunt.initConfig({
+    favicons: {
+      options: {
+        androidHomescreen: true,
+        appleTouchBackgroundColor: '#ffffff',
+        appleTouchPadding: 0,
+        tileBlackWhite: false,
+        tileColor: 'none',
+        trueColor: true
+      },
+      build: {
+        options: {
+          html: 'public/index.html'
+        },
+        src: 'design/icon.png',
+        dest: 'public'
+      }
+    },
+    htmlmin: {
+      build: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          minifyCss: true,
+          minifyJs: true,
+          processConditionalComments: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          removeTagWhitespace: true,
+          sortAttributes: true,
+          sortClassName: true,
+          useShortDoctype: true,
+        },
+        files: {
+          'public/index.html': 'public/index.html'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['favicons', 'htmlmin']);
+
+};
